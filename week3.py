0f4c6377486d715dd05119a1dd4069d995f4e931
natend0 <93206533+natend0@users.noreply.github.com>
2022-02-11 11:09:35 -0500

    Create week3.py

---
+++ week3.py
@@ -0,0 +1,28 @@
+#Extra value calculator
+first = 1 
+second = 2 
+third = 3 
+extra = int(input("What is the extra value?"))
+total = first+second+third+extra 
+print(total)
+
+
+#area of a triangle
+width = int(input("Width?"))
+height = int(input("Height?"))
+area = width*height/5
+print("The area of the triangle is", area)
+
+
+#Convert inches to centemeters
+len_i = int(input("What is the length in inches?")) 
+len_c = len_i*2.54 
+print(len_i, " inches is" ,len_c, "centimeters.")
+
+#Age Prompt
+name = input("What is your name?")
+age = int(input("How old are you?"))  
+print("Hi", name, ", you are", age, "yeas old!")
+
+
+
